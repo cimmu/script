a9368d96c257157ecac9620a18161e85cb84d308
柒慕科技 <92285183+cimmu@users.noreply.github.com>
2021-11-03 11:35:02 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -44,15 +44,18 @@ install_docker() {
     systemctl enable docker  
 }
 install_docker_compose() {
-    sysinfo=$(uname -r)
-    sysver="amd"
-    result=$(echo $sysinfo | grep "${sysver}")
-    if [ "" != "$result" ]; then
-	curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-	chmod +x /usr/local/bin/docker-compose
-    else
-        pip3 install docker-compose
-    fi
+    tag_name=`curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name|cut -f4 -d "\""`
+    curl -L "https://github.com/docker/compose/releases/download/$tag_name/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+#     sysinfo=$(uname -r)
+#     sysver="amd"
+#     result=$(echo $sysinfo | grep "${sysver}")
+#     if [ "" != "$result" ]; then
+# 	curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# 	chmod +x /usr/local/bin/docker-compose
+#     else
+#         pip3 install docker-compose
+#     fi
 }
 
 # 单独检测docker是否安装，否则执行安装docker。
